2fd690d1cb93b2bc866e72dc23ce09040e32ebeb
Abdelbarie Ayouni <abdelbarie.1ayn@gmail.com>
2022-11-16 00:08:47 +0100

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Navbar , NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
+
 
 
 function App() {
@@ -11,7 +13,7 @@ function App() {
           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={DISHES} />
     </div>
   );
 }
